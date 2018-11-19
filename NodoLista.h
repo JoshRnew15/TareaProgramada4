f0907465edfbdae46be71aa61edc2cef7bd250b3
Danielsv12 <40643810+Danielsv12@users.noreply.github.com>
2018-11-19 08:19:51 -0600

    Create NodoLista.h

---
+++ NodoLista.h
@@ -0,0 +1,19 @@
+#include <iostream>
+template <class T>
+class NodoLista {
+    T dato ;
+    NodoLista<T>* siguiente ;
+public:
+    NodoLista(T d){
+        dato= d ;
+        siguiente= nullptr;
+    }
+
+    T getDato() {
+        return dato ;
+    }
+    void setSiguiente(NodoLista<T> *s) {
+        siguiente= s ;
+    }
+    NodoLista<T>* getSiguiente() {return siguiente;}
+};
